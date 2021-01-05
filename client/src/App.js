7ea78ce817b0d43c693f19426d0981adc0a608fa
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2021-01-05 18:46:19 +0200

    Zh222222222222222t Footer

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import HomePage from './Components/HomePage/HomePage'
 import Gallery from './Components/gallery/gallery.jsx'
 import './App.css';
 import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path='/' render={(props) => <HomePage {...props} />} />
         <Route exact path='/gallery' render={(props) => <Gallery {...props} />} />
       </Switch>
+      <Footer />
     </div>
   );
 }

8d9fb72427c09cfb9204d72ce631b3c8dd25b7e7
Moumenh <mohanbali94@gmail.com>
2021-01-05 15:58:04 +0200

    paginations

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import HomePage from './Components/HomePage/HomePage'
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Seeerk</h1>
+      <HomePage />
     </div>
   );
 }

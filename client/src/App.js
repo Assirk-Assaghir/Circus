5f267bfcb643de7bc7e978bca6207f37868a5e61
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2021-01-05 17:32:07 +0200

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './Components/HomePage/HomePage'
 import Gallery from './Components/gallery/gallery.jsx'
 import './App.css';
+import Header from './Components/Header/Header';
 
 
 const App = () => {
   return (
     <div className='App'>
+      <Header />
       <Switch>
         <Route exact path='/' render={(props) => <HomePage {...props} />} />
         <Route exact path='/gallery' render={(props) => <Gallery {...props} />} />
